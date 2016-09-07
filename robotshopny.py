be73150162407eecbb3a92461433db48f8ce5c57
johannabjornholm <johanna.bjornholm@resume.se>
2016-09-07 13:32:24 +0200

    la till klass och konstruktor

--- robotshopny.py
+++ robotshopny.py
@@ -1,4 +1,8 @@
 class Product: 
+	
+	def price_with_tax(self):
+		return self.price * self.count * self.tax
+
 	def __init__(self, price, count,tax):
 		self.price = price
 		self.count = count
@@ -12,4 +16,4 @@ book = Product(price = 100, count = 1, tax = 1.06)
 
 
 
-print(self.price * self.count * self.tax + self.price * self.count * self.tax)
+print(robot.price_with_tax() + book.price_with_tax())
