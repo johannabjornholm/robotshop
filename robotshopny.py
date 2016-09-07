2fd63616aa5c84b5ff37f001601ad976e1b88eb3
johannabjornholm <johanna.bjornholm@resume.se>
2016-09-07 13:19:34 +0200

    klass och metod

--- robotshopny.py
+++ robotshopny.py
@@ -1,1 +1,16 @@
-print(900 * 3 * 1.25 + 100 * 1 * 1.06)
+class Product: 
+	def price_with_tax(self):
+		return self.price * self.count * self.tax
+
+robot = Product()
+robot.price = 900
+robot.count = 2
+robot.tax = 1.25
+
+book = Product()
+book.price = 100
+book.count = 1
+book.tax = 1.06
+
+
+print(robot.price_with_tax() + book.price_with_tax())
